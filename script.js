e1d47d7840529e930467e227a6dae752202703a2
Ariana Osuna <osuna.jenifer@uabc.edu.mx>
2026-02-28 11:24:29 -0800

    Variables

---
+++ script.js
@@ -0,0 +1,49 @@
+console.log("Script file");
+
+//String
+let firstName = "Ariana";
+let lastName = "Osuna";
+
+//Number
+let myAge = 20;
+let height = 1.60;
+
+//Boolean
+let isStudent = true;
+let isProfessor = false;
+
+//Display the values
+console.log(firstName);
+console.log(myAge);
+console.log(isStudent);
+console.log(isProfessor);
+console.log(lastName);
+console.log(height + " meters");
+
+//Aritmetic operations
+let num1 = 10;
+let num2 = 20;
+let pi = 3.1416;
+let r = 3;
+
+let sum = num1 + num2; 
+let sub = num1 - num2;
+let mul = num1 * num2;
+let div = num1 / num2;
+let area = pi * r * r;
+
+//console.log(num1 + "+" + num2 + "=" + sum);
+console.log(`${num1} + ${num2} = ${sum}`);
+console.log(`${num1} - ${num2} = ${sub}`);
+console.log(`${num1} * ${num2} = ${mul}`);
+console.log(`${num1} / ${num2} = ${div}`);
+console.log(`The area of a circle with ${r} = ${area}`);
+
+
+let aVariable = 40;
+aVariable = 50;
+aVariable = 60;
+
+console.log(aVariable);
+
+const weekDays = 7;
