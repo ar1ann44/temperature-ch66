5f07dfd6f5e9da7c794d13af109d38e133851c8b
Ariana Osuna <osuna.jenifer@uabc.edu.mx>
2026-03-03 16:54:09 -0800

    assignment1

---
+++ assignment1.js
@@ -0,0 +1,28 @@
+
+
+let hero = "Emma";
+let activity = "started singing loudly";
+let strangeThing = "a bird";
+let place = "the university";
+let moment = "a quiet Monday morning";
+
+const event = "a secret alien meeting";
+
+console.log(hero + " was visiting " + place + " on " + moment + ".");
+console.log("suddenly, " + hero + " saw " + strangeThing + " and " + activity + ".");
+console.log("It turned out to be " + event + " and everything became chaotic!");
+
+
+
+
+console.log("--- different version of the story ---");
+
+hero = "Natalia";
+activity = "began dancing";
+strangeThing = "a giant robot";
+place = "the zoo";
+moment = "a foggy night";
+
+console.log(hero + " was visiting " + place + " on " + moment + ".");
+console.log("suddenly, " + hero + " saw " + strangeThing + " and " + activity + ".");
+console.log("It turned out to be " + event + " and everything became chaotic!");
